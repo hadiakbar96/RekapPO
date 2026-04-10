42213b313e698bb00765ffa2e0843a5833a4a337
hadialfian <hadiakbar96@gmail.com>
2026-04-10 08:51:40 +0700

    Initial Commit

---
+++ MyOrders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyOrders
+//
+//  Created by Hadi Alfian Akbar on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyOrders/MyOrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOrdersApp.swift
+//  MyOrders
+//
+//  Created by Hadi Alfian Akbar on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyOrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
